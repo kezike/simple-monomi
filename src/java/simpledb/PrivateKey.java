0c654586a1deb0e907fd44b0834bb463cb32bccb
Carlos Henriquez <carlosh@mit.edu>
2017-12-09 11:57:11 -0500

    Add beginnings of Paillier encryption
    TODO: Add these to EncryptedIntegerAggregator
    TODO: Test that the Paillier math actually works and that we get back what we expect

---
+++ src/java/simpledb/PrivateKey.java
@@ -0,0 +1,28 @@
+package simpledb;
+
+import java.math.BigInteger;
+
+/**
+ * A class that represents the private part of the Paillier key pair.
+ * 
+ * Borrowed from https://github.com/kunerd/jpaillier
+ */
+class PrivateKey {
+
+    private final BigInteger lambda;
+    private final BigInteger preCalculatedDenominator;
+
+    PrivateKey(BigInteger lambda, BigInteger preCalculatedDenominator) {
+        this.lambda = lambda;
+
+        this.preCalculatedDenominator = preCalculatedDenominator;
+    }
+
+    public BigInteger getLambda() {
+        return lambda;
+    }
+
+    public BigInteger getPreCalculatedDenominator() {
+        return preCalculatedDenominator;
+    }
+}
