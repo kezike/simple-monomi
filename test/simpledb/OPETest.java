725f47e42e46d4eed0c5222f08bfc7392a487def
Kayode Ezike <kayodeezike@Kayodes-MacBook-Pro.local>
2017-12-10 19:11:33 -0500

    Added some basic OPE functionality

---
+++ test/simpledb/OPETest.java
@@ -0,0 +1,10 @@
+package simpledb;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import junit.framework.JUnit4TestAdapter;
+
+public class OPETest {
+}
