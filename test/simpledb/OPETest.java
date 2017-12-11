58494107189323a5e19ede8b6a9bc90c2786e4b8
Kayode Ezike <kayodeezike@31-39-160.wireless.csail.mit.edu>
2017-12-11 17:51:05 -0500

    Fixed test mistake that was originally reading from empty file

--- test/simpledb/OPETest.java
+++ test/simpledb/OPETest.java
@@ -3,6 +3,8 @@ package simpledb;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import junit.framework.JUnit4TestAdapter;
 import java.io.File;
@@ -26,7 +28,8 @@ public class OPETest {
     /**
      * Initialize each OPE unit test
      */
-    @Before public void setupOPETest() throws Exception {
+    @Before
+    public void setupOPETest() throws Exception {
         this.add_15 = new OPE_Cipher.Add(15);
         this.sub_3 = new OPE_Cipher.Sub(3);
         this.mult_7 = new OPE_Cipher.Mult(7);
@@ -42,12 +45,14 @@ public class OPETest {
         Type types[] = new Type[]{ Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE };
         String names[] = new String[]{ "field0", "field1", "field2" };
         TupleDesc td = new TupleDesc(types, names);
-        this.table = new HeapFile(new File("ope_test.dat"), td);
-        Database.getCatalog().addTable(table, "ope_test");
-        this.table_iter = table.iterator(new TransactionId());
+        this.table = new HeapFile(new File("test/simpledb/ope_test.dat"), td);
+        Catalog catalog = Database.getCatalog();
+        catalog.addTable(this.table, "ope_test");
+        this.table_iter = this.table.iterator(new TransactionId());
     }
 
-    @Test public void testOPEAdd() throws DbException, TransactionAbortedException {
+    @Test
+    public void testOPEAdd() throws DbException, TransactionAbortedException {
         OPE_PrivateKey priv_key = new OPE_PrivateKey(this.ope_add_15);
         OPE_PublicKey pub_key = new OPE_PublicKey(this.ope_add_15);
         OPE_KeyPair key_pair = new OPE_KeyPair(priv_key, pub_key, this.ope_add_15);
@@ -62,12 +67,48 @@ public class OPETest {
               IntField int_field = (IntField) field;
               Integer val_orig = int_field.getValue();
               Integer val_enc = pub_key.encrypt(val_orig);
-              Integer val_dec = key_pair.decrypt(val_orig);
+              Integer val_dec = key_pair.decrypt(val_enc);
+              assertFalse(val_orig.equals(val_enc));
+              assertFalse(val_enc.equals(val_dec));
               assertEquals(val_orig, val_dec);
             }
           }
         }
         this.table_iter.rewind();
         this.table_iter.close();
+    }
+
+    @Test
+    public void testOPESub() throws DbException, TransactionAbortedException {
+        OPE_PrivateKey priv_key = new OPE_PrivateKey(this.ope_sub_3);
+        OPE_PublicKey pub_key = new OPE_PublicKey(this.ope_sub_3);
+        OPE_KeyPair key_pair = new OPE_KeyPair(priv_key, pub_key, this.ope_sub_3);
+        this.table_iter.open();
+        while (this.table_iter.hasNext()) {
+          Tuple tuple = this.table_iter.next();
+          TupleDesc tuple_desc = tuple.getTupleDesc();
+          int num_fields = tuple_desc.numFields();
+          for (int i = 0; i < num_fields; i++) {
+            Field field = tuple.getField(i);
+            if (field.getType() == Type.INT_TYPE) {
+              IntField int_field = (IntField) field;
+              Integer val_orig = int_field.getValue();
+              Integer val_enc = pub_key.encrypt(val_orig);
+              Integer val_dec = key_pair.decrypt(val_enc);
+              assertFalse(val_orig.equals(val_enc));
+              assertFalse(val_enc.equals(val_dec));
+              assertEquals(val_orig, val_dec);
+            }
+          }
+        }
+        this.table_iter.rewind();
+        this.table_iter.close();
+    }
+
+    /**
+     * JUnit suite target
+     */
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(OPETest.class);
     }
 }
